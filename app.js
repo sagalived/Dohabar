50d712a60873dd5df3a0b0659987bed13499d7de
Junior Morais <sagalived@gmail.com>
2022-09-27 04:34:32 -0300

    update
    finalmente

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const logo = document.querySelector('.header .background .nav-list .logo .yout');
-const mobile_menu = document.querySelector('.header .yout .background .nav ul');
-const menu_item = document.querySelectorAll('.header .yout .background .nav ul li a');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-logo.addEventListener('click', () => {
-	logo.classList.toggle('active');
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
